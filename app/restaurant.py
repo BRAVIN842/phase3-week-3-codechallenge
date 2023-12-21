bcf3351e1e00fe21179928dbe4a56ca202276a99
Bravin <onsasebravin853@gmail.com>
2023-12-21 14:50:42 +0300

    base

--- app/restaurant.py
+++ app/restaurant.py
@@ -2,3 +2,7 @@ from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship, Session
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.sql import desc
+
+Base = declarative_base()
+engine = create_engine('sqlite:///restaurant.db')
+session = Session(engine)
