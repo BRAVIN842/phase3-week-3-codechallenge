61d8a86c0b1652ce95297c8f574d129a4ae90711
Bravin <onsasebravin853@gmail.com>
2023-12-21 14:52:54 +0300

    Customer class

--- app/restaurant.py
+++ app/restaurant.py
@@ -6,3 +6,33 @@ from sqlalchemy.sql import desc
 Base = declarative_base()
 engine = create_engine('sqlite:///restaurant.db')
 session = Session(engine)
+
+class Customer(Base):
+    __tablename__ = 'customers'
+
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+
+    reviews = relationship('Review', back_populates='customer')
+
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def favorite_restaurant(self):
+        # Find the restaurant with the highest star rating from this customer's reviews
+        highest_rated_review = max(self.reviews, key=lambda review: review.star_rating, default=None)
+        return highest_rated_review.restaurant if highest_rated_review else None
+
+    def add_review(self, restaurant, rating):
+        # Create a new review for the restaurant with the given rating
+        new_review = Review(customer=self, restaurant=restaurant, star_rating=rating)
+        session.add(new_review)
+        session.commit()
+
+    def delete_reviews(self, restaurant):
+        # Delete all reviews for the given restaurant
+        for review in self.reviews:
+            if review.restaurant == restaurant:
+                session.delete(review)
+        session.commit()
